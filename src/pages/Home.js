b05d413f514e2af698f8dabcf1068bbd4d044cab
mr-blankson <fat_tonyy@icloud.com>
2020-02-13 11:23:51 +0000

    useEffect , API setup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,12 @@ export default function Home() {
   const [loading, setLoading] = React.useState(false);
   const [searchTerm, setSearchTerm] = React.useState("a");
   const [cocktails, setCocktails] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
   return (
     <main>
       <SearchForm setSearchTerm={setSearchTerm} />
