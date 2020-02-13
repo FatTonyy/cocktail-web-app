3735c2f291320467115b8382e757a5d8595288be
mr-blankson <fat_tonyy@icloud.com>
2020-02-13 10:39:04 +0000

    about-page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Salvia beard messenger bag helvetica cold-pressed. Synth put a bird on
+        it fam tattooed, shoreditch hammock meggings organic sustainable you
+        probably haven't heard of them disrupt marfa. Tacos keffiyeh polaroid,
+        live-edge actually affogato chillwave viral. Disrupt cliche sriracha
+        gastropub tousled hexagon. Truffaut deep v butcher seitan 8-bit neutra
+        hammock humblebrag forage PBR&B echo park poutine direct trade.
+        Vaporware man bun microdosing banjo 3 wolf moon lyft poke. Swag tote bag
+        scenester hella prism dreamcatcher yr try-hard pinterest echo park
+        selfies kitsch artisan crucifix organic.
+      </p>
+    </section>
+  );
 }
