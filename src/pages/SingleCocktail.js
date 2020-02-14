4ac2b2cebc03a71619f23089a4c9c298b6c2c7c4
mr-blankson <fat_tonyy@icloud.com>
2020-02-14 19:32:23 +0000

    single-cocktail-http-request

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -2,7 +2,60 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 export default function SingleCocktail() {
-  console.log(useParams);
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
+
+  React.useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+        );
+        const data = await response.json();
+        if (data.drinks) {
+          const {
+            strDrink: name,
+            strDrinkThumb: image,
+            strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            strInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5
+          } = data.drinks[0];
+
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5
+          ];
+
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            instructions,
+            ingredients
+          };
+          setCocktail(newCocktail);
+        } else {
+          setCocktail(null);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getCocktail();
+  }, [id]);
 
   return <h1>single cocktail page</h1>;
 }
